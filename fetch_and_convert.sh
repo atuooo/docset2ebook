eaa1e3bf55dca4e1bf4feb5450656245757b4dbb
Atuooo <aaatuooo@gmail.com>
2016-11-01 17:33:39 +0800

    add KindleGen

--- fetch_and_convert.sh
+++ fetch_and_convert.sh
@@ -1,12 +1,15 @@
 ##!/bin/sh
-FORMAT=epub
+FORMAT=mobi
 OUTPUT_DIR=~/Documents/ADCBooks
 COMMAND="python docset2kindle.py"
 mkdir $OUTPUT_DIR
 
-$COMMAND /Developer/Documentation/DocSets -o $OUTPUT_DIR/XCode -f $FORMAT
-$COMMAND /Library/Developer/Shared/Documentation/DocSets/com.apple.adc.documentation.AppleSnowLeopard.CoreReference.docset -o $OUTPUT_DIR/OSX  -f $FORMAT
+## $COMMAND /Developer/Documentation/DocSets -o $OUTPUT_DIR/XCode -f $FORMAT
 
-wget http://devimages.apple.com/docsets/20110720/com.apple.adc.documentation.AppleiOS4_3.iOSLibrary.xar
-xar -xf com.apple.adc.documentation.AppleiOS4_3.iOSLibrary.xar
-$COMMAND com.apple.adc.documentation.AppleiOS4_3.iOSLibrary.docset -o $OUTPUT_DIR/iOS -f $FORMAT
+# ~/Library/Developer/Shared/Documentation/DocSets/
+$COMMAND /Users/Atuooo/Library/Developer/Shared/Documentation/DocSets/com.apple.adc.documentation.iOS.docset -o $OUTPUT_DIR/Xcode -f $FORMAT
+
+
+## wget http://devimages.apple.com/docsets/20110720/com.apple.adc.documentation.AppleiOS4_3.iOSLibrary.xar
+## xar -xf com.apple.adc.documentation.AppleiOS4_3.iOSLibrary.xar
+## $COMMAND com.apple.adc.documentation.AppleiOS4_3.iOSLibrary.docset -o $OUTPUT_DIR/iOS -f $FORMAT
